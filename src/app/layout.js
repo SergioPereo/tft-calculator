245dc6282e7b05cc77210599d4fc601f42a62d59
SergioPereo <sergio.pereo@outlook.com>
2023-12-21 19:14:49 -0600

    vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from "./components/Navbar.js"
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <Navbar suppressHydrationWarning />
         {children}
       </body>
